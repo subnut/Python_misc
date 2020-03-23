dda6c09371a9a471c9fb20bc10c17a93e58a7656
Subhaditya Nath <sn03.general@gmail.com>
2020-03-24 00:41:27 +0530

    modified:   index.html 	modified:   index_html_creator.py

--- index_html_creator.py
+++ index_html_creator.py
@@ -15,15 +15,28 @@ l=[x for x in listdir() if (x[-3:]=='.py' and x!='index_html_creator.py')]
 
 h.write('<br>')
 
+h.write('<ul>')
+for a in range(len(l)):
+    h.write('<li>')
+    h.write('<a href="#'+str(a)+'">'+l[a][:-3]+'</a>')
+    h.write('</li>')
+h.write('</ul>')
+
+h.write('<br><br>')
+
+id=-1
+
 for z in l:
+    id+=1
+    h.write('<a id="'+str(id)+'"></a>')
+    h.write('<br><br>')
     h.write(z[:-3]+'<br>')
     h.write('''<pre>
 <code class=" language-python">''')
     h.writelines(contents(z))
     h.write('''<br>
 </code>
-</pre>
-<br><br>''')
+</pre>''')
 
 h.write('''<script src="prism.js"></script>
 </body>
