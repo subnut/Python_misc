671488e2e882149aa4e9975f7a91e9777e357781
Subhaditya Nath <sn03.general@gmail.com>
2020-03-23 20:39:31 +0530

    modified:   README.md 	modified:   aufbauchart.py 	modified:   index.html 	new file:   index_html_creator.py 	modified:   prism.css 	modified:   prism.js

---
+++ index_html_creator.py
@@ -0,0 +1,32 @@
+h=open('index.html','w')
+h.write('''<html>
+<head>
+<title>Python_misc</title>
+<link rel="stylesheet" href="prism.css">
+</head>
+<body bgcolor="#F5F2F0">''')
+
+def contents(f):
+    with open(f,'r') as file:
+        return file.readlines()
+
+from os import listdir
+l=[x for x in listdir() if x[-3:]=='.py']
+
+h.write('<br>')
+
+for z in l:
+    h.write(z[:-3]+'<br>')
+    h.write('''<pre>
+<code class=" language-python">
+<br>''')
+    h.writelines(contents(z))
+    h.write('''<br>
+</code>
+</pre>
+<br><br>''')
+
+h.write('''<script src="prism.js"></script>
+</body>
+</html>''')
+h.close()
