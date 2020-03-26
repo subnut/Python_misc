0e68bb1267ce41739834ec92501045d5f0c53c65
Subhaditya Nath <sn03.general@gmail.com>
2020-03-26 12:15:26 +0530

    major typo

--- aufbauchart.py
+++ aufbauchart.py
@@ -1,6 +1,6 @@
 ## Aufbau Chart
 
-l='spdf'+#'ghijklmnopqrstuvwxyz'        #remove the first hashtag in this line to print upto the Z-orbital
+l='spdf'#+'ghijklmnopqrstuvwxyz'        #remove the first hashtag in this line to print upto the Z-orbital
 #l=l.upper()                            #remove the first hashtag in this line to print the orbital letters in UPPERCASE
 n=[None]
 for z in range(1,len(l)+1):
