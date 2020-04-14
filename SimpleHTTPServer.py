9944bce332ac4838448a6e5aa9c4cfaada64e528
Subhaditya Nath <sn03.general@gmail.com>
2020-04-14 23:26:17 +0530

    SimpleHTTPServer

---
+++ SimpleHTTPServer.py
@@ -0,0 +1,23 @@
+''' 
+This is not actually a Python program
+But it is, undeniably, an amazing piece of code
+N.B. Python2 must be installed on the machine
+
+At the terminal, cd into the folder you want
+run the following command
+
+python2 -m SimpleHTTPServer 8080
+
+then, it will set up a simple HTTP server on port 8080
+sharing the contents of the folder
+
+Explanation of the code -
+
+This command executes Python2
+and imports the embedded Python2 module SimpleHTTPServer
+which runs an HTTP server on port 8080 of the machine
+on the directory this code is executed in
+
+Simple way to quickly send some files :D
+
+'''
