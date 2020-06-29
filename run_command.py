04b69ddc54085cdbbcbdd0cb5189cd0cf107a2ba
subnut <sn03.general@gmail.com>
2020-06-30 00:18:00 +0530

    fixed run_command

--- run_command.py
+++ run_command.py
@@ -1,9 +1,8 @@
-## Used to run commands input as a single string
+## Used to run commands input as a single string using "sh -c"
 
 def run(command):
-    command=command.split()
     from subprocess import PIPE,Popen
-    process=Popen(command,stdout=PIPE,stderr=PIPE,text=True)
+    process=Popen(["sh","-c",command],stdout=PIPE,stderr=PIPE,text=True)
     stdout,stderr=process.communicate()
     exitcode=process.returncode
     return {'stdout':stdout,'stderr':stderr,'exitcode':exitcode}
