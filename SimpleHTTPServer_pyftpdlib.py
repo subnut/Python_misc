ced9cf9c918a6e4c508b2c00cb5d9be8a988a21c
subnut <sn03.general@gmail.com>
2020-07-14 16:06:25 +0530

    dunno what ....

--- SimpleHTTPServer_pyftpdlib.py
+++ SimpleHTTPServer_pyftpdlib.py
@@ -1,31 +1,33 @@
-''' 
-This is not actually a Python program
-But it is, undeniably, an amazing piece of code
-N.B. Python2 must be installed on the machine
-
-At the terminal, cd into the folder you want
-run the following command
-
-python2 -m SimpleHTTPServer 8080
-
-then, it will set up a simple HTTP server on port 8080
-sharing the contents of the folder
-
-Explanation of the code -
-
-This command executes Python2
-and imports the embedded Python2 module SimpleHTTPServer
-which runs an HTTP server on port 8080 of the machine
-on the directory this code is executed in
-
-Simple way to quickly send some files :D
-
-
-also, for ftp server (again python2)
-
-pip install pyftpdlib
-python2 -m pyftpdlib --help
-
-see the help and configure as required :D
-
-'''
+#  This is not actually a Python program
+#  But it is, undeniably, an amazing piece of code
+#
+#  At the terminal, cd into the folder you want
+#  run the following command
+#
+#  python -m http.server 8080
+#
+#  then, it will set up a simple HTTP server on port 8080
+#  sharing the contents of the folder
+#
+#  Explanation of the code -
+#
+#  This command executes Python
+#  and imports the embedded Python2 module SimpleHTTPServer
+#  which runs an HTTP server on port 8080 of the machine
+#  on the directory this code is executed in
+#
+#  Simple way to quickly send some files :D
+#
+#  P.S. For sending larger files,
+#  it is better to use a server having 'range' support
+#  So, do this -
+#       pip install rangehttpserver
+#  Then, run - python -m RangeHTTPServer
+#  Note: It will ONLY serve on the port 8000
+#
+#  also, for ftp server
+#
+#  pip install pyftpdlib
+#  python -m pyftpdlib --help
+#
+#  see the help and configure as required :D
