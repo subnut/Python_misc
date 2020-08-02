13a8cd88bdfa20566cb6a0e88d06687fcad96844
subnut <sn03.general@gmail.com>
2020-08-02 21:14:54 +0530

    updated run_command, added gtk_theme_change_reload

--- run_command.py
+++ run_command.py
@@ -1,9 +1,8 @@
-## Used to run commands input as a single string using "sh -c"
+# Used to run commands input as a single string using "sh -c"
 
 def run(command):
-    from subprocess import PIPE,Popen
-    process=Popen(["sh","-c",command],stdout=PIPE,stderr=PIPE,text=True)
-    stdout,stderr=process.communicate()
-    exitcode=process.returncode
-    return {'stdout':stdout,'stderr':stderr,'exitcode':exitcode}
-
+    from subprocess import PIPE, Popen
+    process = Popen(["sh",  "-c", command], stdout=PIPE, stderr=PIPE, text is True)
+    stdout, stderr = process.communicate()
+    exitcode = process.returncode
+    return {'stdout': stdout, 'stderr': stderr, 'exitcode': exitcode}
