00d8d2f2d73bbce6385c9e30cbd9254d055d7ded
subnut <sn03.general@gmail.com>
2020-08-10 22:10:32 +0530

    rm ansi_color_print.py

--- ansi_color_print.py
+++
@@ -1,7 +0,0 @@
-def colorPrint(string,colorArgs,end='\n'):
-    import sys
-    fore=back=None
-    try: fore=colorArgs[0]
-    except: pass
-    try: back=colorArgs[1]
-    sys.stdout.write('\033[38;5;'+str(foreColor)+'m'+string+'')
